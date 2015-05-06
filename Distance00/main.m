2262160e00279af3869580e928383885b8f388a2
Your Name <everettchurchey@crbc.edu>
2015-05-06 09:54:06 -0400

    Distance00

--- Distance00/main.m
+++ Distance00/main.m
@@ -16,26 +16,38 @@ int main(int argc, const char * argv[]) {
         point * p2 = [point alloc];
         p2 = [p2 init:0 y:0];
         [p1 distance: p2];
+        
+ 
 
         const float ERROR_THRESHOLD=0.05;
         if ([p1 distance: p2]- 0.0 > ERROR_THRESHOLD){
             NSLog (@"Invalid Answer");
         }
-//        if (distance(0, 0, 0, 0) != 0){
-//            NSLog(@"Invalid Answer");
-//        }
-//        if (distance(0, 0, 1, 1) -1.41 > ERROR_THRESHOLD){
-//            NSLog(@"Invalid Answer");
-//        }
-//        if (distance(0, 0, 5, 5) -7.07 > ERROR_THRESHOLD){
-//            NSLog(@"Invalid Answer");
-//        }
-//        if (distance(1, 1, 5, 6) -6.40 > ERROR_THRESHOLD){
-//            NSLog(@"Invalid Answer");
-//        }
-//        if (distance(7, 10, 16, 13) -9.49 > ERROR_THRESHOLD){
-//            NSLog(@"Invalid Answer");
+        p1 = [p1 init:0 y:0];
+        p2 = [p2 init:1 y:1];
         
+        //p1 replaces self in .m
+        if ([p1 distance: p2] - 1.41 >ERROR_THRESHOLD){
+            NSLog(@"Invalid Answer");
+        }
+        p1 = [p1 init:0 y:0];
+        p2 = [p2 init:5 y:5];
+
+        if ([p1 distance: p2]-7.07 > ERROR_THRESHOLD){
+            NSLog(@"Invalid Answer");
+        }
+        p1 = [p1 init:1 y:1];
+        p2 = [p2 init:5 y:6];
+
+        if ([p1 distance: p2] -6.40 > ERROR_THRESHOLD){
+            NSLog(@"Invalid Answer");
+       }
+        p1 = [p1 init: 7 y: 10];
+        p2 = [p2 init: 16 y: 13];
+        
+        if ([p1 distance: p2] -9.49 > ERROR_THRESHOLD){
+            NSLog(@"Invalid Answer");
+        }
     }
     return 0;
     }
