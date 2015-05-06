50c38b256333dad0dc2e19850d204d13eb1db645
Your Name <everettchurchey@crbc.edu>
2015-05-06 09:02:38 -0400

    distance00

--- Distance00/main.m
+++ Distance00/main.m
@@ -18,23 +18,23 @@ int main(int argc, const char * argv[]) {
         [p1 distance: p2];
 
         const float ERROR_THRESHOLD=0.05;
-        if (distance(0, 0, 1, 0)- 1.0 > ERROR_THRESHOLD){
+        if ([p1 distance: p2]- 0.0 > ERROR_THRESHOLD){
             NSLog (@"Invalid Answer");
         }
-        if (distance(0, 0, 0, 0) != 0){
-            NSLog(@"Invalid Answer");
-        }
-        if (distance(0, 0, 1, 1) -1.41 > ERROR_THRESHOLD){
-            NSLog(@"Invalid Answer");
-        }
-        if (distance(0, 0, 5, 5) -7.07 > ERROR_THRESHOLD){
-            NSLog(@"Invalid Answer");
-        }
-        if (distance(1, 1, 5, 6) -6.40 > ERROR_THRESHOLD){
-            NSLog(@"Invalid Answer");
-        }
-        if (distance(7, 10, 16, 13) -9.49 > ERROR_THRESHOLD){
-            NSLog(@"Invalid Answer");
+//        if (distance(0, 0, 0, 0) != 0){
+//            NSLog(@"Invalid Answer");
+//        }
+//        if (distance(0, 0, 1, 1) -1.41 > ERROR_THRESHOLD){
+//            NSLog(@"Invalid Answer");
+//        }
+//        if (distance(0, 0, 5, 5) -7.07 > ERROR_THRESHOLD){
+//            NSLog(@"Invalid Answer");
+//        }
+//        if (distance(1, 1, 5, 6) -6.40 > ERROR_THRESHOLD){
+//            NSLog(@"Invalid Answer");
+//        }
+//        if (distance(7, 10, 16, 13) -9.49 > ERROR_THRESHOLD){
+//            NSLog(@"Invalid Answer");
         
     }
     return 0;
